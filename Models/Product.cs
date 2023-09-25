58c0e2541a6d8299f7a67bf70175eba0c3889ea1
Kenny Heagle <kennyheagle@gmail.com>
2023-09-25 09:56:45 -0700

    initialize product attributes to default

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,14 +6,14 @@ namespace crafts.WebSite.Models
 {
     public class Product
     {
-        public string Id { get; set; }
-        public string Maker { get; set; }
+        public string Id { get; set; } = default!;
+        public string Maker { get; set; } = default!;
         [JsonPropertyName("img")]
-        public string Image { get; set; }
-        public string Url { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public int[] Ratings { get; set; }
+        public string Image { get; set; } = default!;
+        public string Url { get; set; } = default!;
+        public string Title { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public int[] Ratings { get; set; } = default!;
 
         public override string ToString() => JsonSerializer.Serialize<Product>(this);
     }
